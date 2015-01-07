ea16238e1953f3e35cd02c84e6900751d9511613
Marc S. Brooks <marc@mbrooks.info>
2015-01-06 21:39:58 -0800

    Updated copyright year
    Added "License and Warranty" to .rdoc

--- jquery.ez.menu.js
+++ jquery.ez.menu.js
@@ -2,7 +2,7 @@
  *  EZ Menu
  *  An easy way to create a navigation menu from JSON
  *
- *  Copyright 2012-2014, Marc S. Brooks (http://mbrooks.info)
+ *  Copyright 2012-2015, Marc S. Brooks (http://mbrooks.info)
  *  Licensed under the MIT license:
  *  http://www.opensource.org/licenses/mit-license.php
  *
